ea0bd0d9b2499e4f1c87905294aa80942548715e
jorgecedeno <jorgecedeno26@gmail.com>
2018-05-14 16:34:43 -0400

    Add files via upload
    Funcionamiento del Sendchar y el Recvchar con el codigo del PWM

--- FLASH/Generated_Code/Bit1_c.d
+++ FLASH/Generated_Code/Bit1_c.d
@@ -1,8 +1,8 @@
-Sources/componentes/sharp_c.obj: C:\Users\user.LC\workspace\Crusher\Sources\componentes\sharp.c \
+Generated_Code/Bit1_c.obj: C:\Users\user.LC\workspace\Crusher\Generated_Code\Bit1.c \
 	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
 	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
 	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Users\user.LC\workspace\Crusher\Generated_Code\Cpu.h \
+	C:\Users\user.LC\workspace\Crusher\Generated_Code\Bit1.h \
 	C:\Users\user.LC\workspace\Crusher\Generated_Code\PE_Types.h \
 	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\stdint.h \
 	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ansi_parms.h \
@@ -17,5 +17,4 @@ Sources/componentes/sharp_c.obj: C:\Users\user.LC\workspace\Crusher\Sources\comp
 	C:\Users\user.LC\workspace\Crusher\Generated_Code\PE_Const.h \
 	C:\Users\user.LC\workspace\Crusher\Generated_Code\IO_Map.h \
 	C:\Users\user.LC\workspace\Crusher\Generated_Code\PE_Timer.h \
-	C:\Users\user.LC\workspace\Crusher\Sources\componentes\sharp.h \
-	C:\Users\user.LC\workspace\Crusher\Generated_Code\SHARPadc.h 
+	C:\Users\user.LC\workspace\Crusher\Generated_Code\Cpu.h 
